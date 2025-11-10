1fb0e09d3a3a42f513eb251e249bd1efd444eb68
caltuntas <cihat.altuntas@gmail.com>
2025-11-10 16:59:28 +0300

    parse memory maps and read rw sections

--- keyfinder.c
+++ keyfinder.c
@@ -2,18 +2,23 @@
 #include<stdlib.h>
 #include<fcntl.h>
 #include<unistd.h>
+#include<string.h>
 #include<sys/stat.h>
 #include<sys/types.h>
 #include <sys/ptrace.h>
 #include <sys/wait.h>
 
 
+//TODO:graceful exit and resource clean-up
 int main()
 {
   int pid=323339;
-  char file[64] = {0};
-  sprintf(file, "/proc/%ld/mem",(long)pid);
-  printf("file name is %s",file);
+  char mem_file[64] = {0};
+  char maps_file[64] = {0};
+  sprintf(mem_file, "/proc/%ld/mem",(long)pid);
+  sprintf(maps_file, "/proc/%ld/maps",(long)pid);
+  printf("mem_file name is %s",mem_file);
+  printf("maps_file name is %s",maps_file);
 
   long ptrace_res = ptrace(PTRACE_ATTACH,pid,NULL,NULL);
   if(ptrace_res==-1) {
@@ -22,28 +27,51 @@ int main()
   }
   waitpid(pid,NULL,0);
 
-  int fd =open(file,O_RDONLY);
-  if(fd==-1) {
+  int mem_fd =open(mem_file,O_RDONLY);
+  if(mem_fd==-1) {
     perror("open");
     return EXIT_FAILURE;
   }
 
-  char buf[1024];
-  unsigned long offset = 0x55b3e9024000UL;
-  int seek_result = lseek(fd,offset,SEEK_SET);
-  if (seek_result == -1) {
-    perror("lseek");
+  FILE *maps_file_ptr =fopen(maps_file,"r");
+  if (maps_file_ptr == NULL) {
+    perror("fopen");
     return EXIT_FAILURE;
   }
 
-  int read_result = read(fd,buf,sizeof(buf));
+  int buf_len=255;
+  char buffer[buf_len];
+  while(fgets(buffer,buf_len,maps_file_ptr)) {
+    unsigned long start_addr;
+    unsigned long end_addr;
+    char perms[5]={0};
+    sscanf(buffer,"%lx-%lx %s\n",&start_addr,&end_addr,perms);
+    //printf("start=%lu,end=%lu,perms=%s\n",start_addr,end_addr,perms);
 
-  if(read_result==-1) {
-    perror("read");
-    return EXIT_FAILURE;
+    if (strstr(perms,"rw")!=NULL){
+      printf("line---%s\n",buffer);
+      //printf("read write section---");
+      char buf[1024];
+      unsigned long offset = start_addr;
+      int seek_result = lseek(mem_fd,offset,SEEK_SET);
+      if (seek_result == -1) {
+	perror("lseek");
+	return EXIT_FAILURE;
+      }
+
+      int read_result = read(mem_fd,buf,sizeof(buf));
+
+      if(read_result==-1) {
+	perror("read");
+	return EXIT_FAILURE;
+      }
+    }
   }
 
-  if(close(fd)==-1) {
+  fclose(maps_file_ptr);
+
+
+  if(close(mem_fd)==-1) {
     perror("close");
     return EXIT_FAILURE;
   }
