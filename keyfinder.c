8dca9dcbf47f6137b2f33a511b85902c7544d28b
caltuntas <cihat.altuntas@gmail.com>
2025-11-19 14:41:01 +0300

    perf:aes 128 key scan function

--- keyfinder.c
+++ keyfinder.c
@@ -24,30 +24,20 @@ key_search_result_t check_aes_128_key_expantion(uint8_t *buffer,size_t size,uint
 {
   key_search_result_t res = {false,0,0,{0}};
   uint8_t all_keys[176]={0};
-  uint8_t first_key[16]={0};
+  uint8_t key[16]={0};
   for (int i=0; i<size-175; i++) {
-    //printf("buffer window=%d\n",i);
-    uint8_t expanded_key[16]={0};
-    memcpy(first_key,buffer+i,16);
-    //printf("candidate key=");
-    //print_hex(first_key,16);
-    memset(all_keys,0,176);
-    memcpy(all_keys,first_key,16);
-    memcpy(expanded_key,first_key,16);
+    memcpy(key,buffer+i,16);
+    memcpy(all_keys,key,16);
     for (int round=1; round<11; round++) {
-      expand_key(round,expanded_key);
-      //printf("expanded key=");
-      //print_hex(expanded_key,16);
-      memcpy(all_keys+(round*16),expanded_key,16);
-      //printf("all keys=");
-      //print_hex(all_keys,16*(round+1));
+      expand_key(round,key);
+      memcpy(all_keys+(round*16),key,16);
       if(memcmp(buffer+i,all_keys,round*16+16)!=0)
         break;
       if (round==10) {
         res.found =true;
         res.offset=i;
         res.address = base_addr+i;
-        memcpy(res.key,first_key,16); 
+        memcpy(res.key,all_keys,16); 
         return res;
       }
     }
